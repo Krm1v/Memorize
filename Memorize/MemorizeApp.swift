03b3f749fa60e5b14c27668d0482b374b13ca921
Krm1v <vladkarmalov@gmail.com>
2024-04-03 00:15:31 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Владислав Баранкевич on 03.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
